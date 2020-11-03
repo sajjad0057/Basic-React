8bfcb9ed3864c478fae52a8f281b0ec792f2d6e6
sajjad Hossain <sajjadhossain0057@gamil.com>
2020-11-03 14:40:19 +0600

    start Using react-codemode

--- src/components/lists/bookList.jsx
+++ src/components/lists/bookList.jsx
@@ -7,14 +7,36 @@ class BookList extends Component{
         super(props)
         console.log("BookList Constructor !");
     }
+    componentWillMount(){
+        console.log("BookList Component componentWillMount !");
+        }
 
 
     componentDidMount(){
         console.log("BookList Component componentDidMount !");
       }
 
-    componentWillMount(){
-    console.log("BookList Component componentWillMount !");
+    componentWillReceiveProps(nextProps){
+        console.log("Update bookList componentWillReceiveProps" , nextProps);
+    }
+    /*
+    shouldComponentUpdate(nextProps, nextState) −
+    This method returns a Boolean value true or false. Based on the return value React executes the render method which updates the actual DOM.
+    We can add custom logic if requires in this method like comparing some variables to decide the process of updating.
+    Side effects should be avoided.
+    
+    */
+
+    shouldComponentUpdate(nextProps,nextState){
+        console.log("Update bookList shouldComponentUpdate",nextProps,nextState);
+        return true
+    }
+    componentWillUpdate(nextProps,nextState){
+        console.log("update bookList componentWillUpdate !",nextProps,nextState);
+    }
+
+    componentDidUpdate(){
+        console.log("update bookList componentDidUpdate !");
     }
      
     render(){
